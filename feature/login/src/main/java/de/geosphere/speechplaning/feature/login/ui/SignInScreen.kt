9aad56272ba24c9cc571c148c2e3fedd58ba4023
Werner Schröter <werner.schroeter@geosphere.de>
2025-11-11 19:22:03 +0100

    LoginScreen.kt and his subscreens SignInScreen.kt, SignUpScreen.kt now with previews

--- feature/login/src/main/java/de/geosphere/speechplaning/feature/login/ui/SignInScreen.kt
+++ feature/login/src/main/java/de/geosphere/speechplaning/feature/login/ui/SignInScreen.kt
@@ -1,5 +1,7 @@
 package de.geosphere.speechplaning.feature.login.ui
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -14,10 +16,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import de.geosphere.speechplaning.core.ui.atoms.di.PreviewKoin
+import de.geosphere.speechplaning.theme.SpeechPlaningTheme
+import de.geosphere.speechplaning.theme.ThemePreviews
 
 @Composable
-fun SignInScreen(
-    authViewModel: AuthViewModel,
+fun ColumnScope.SignInScreen(
+    onSignInClick: (String, String) -> Unit,
     onGoToSignUp: () -> Unit // Callback für den Wechsel
 ) {
     var email by remember { mutableStateOf("") }
@@ -39,7 +44,7 @@ fun SignInScreen(
     )
     Spacer(modifier = Modifier.height(16.dp))
     Button(
-        onClick = { authViewModel.signInWithEmailAndPassword(email, password) },
+        onClick = { onSignInClick(email, password) },
         modifier = Modifier.fillMaxWidth()
     ) {
         Text("Anmelden")
@@ -47,5 +52,18 @@ fun SignInScreen(
     Spacer(modifier = Modifier.height(8.dp))
     TextButton(onClick = onGoToSignUp) {
         Text("Noch kein Konto? Jetzt registrieren")
+    }
+}
+
+@ThemePreviews
+@Composable
+fun SignInScreenPreview() = PreviewKoin {
+    SpeechPlaningTheme {
+        Column() {
+            SignInScreen(
+                onSignInClick = { _, _ -> },
+                onGoToSignUp = {}
+            )
+        }
     }
 }
