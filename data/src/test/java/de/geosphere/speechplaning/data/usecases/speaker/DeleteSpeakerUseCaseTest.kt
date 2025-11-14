86c40bb76b1c911b39b1536b65e13b28eaf07251
Werner Schröter <werner.schroeter@geosphere.de>
2025-11-14 16:08:44 +0100

    SPEECHPLAN-25 feature: Jahresplaner implementieren -vorbereitung für module speeches und congregations

---
+++ data/src/test/java/de/geosphere/speechplaning/data/usecases/speaker/DeleteSpeakerUseCaseTest.kt
@@ -0,0 +1,49 @@
+package de.geosphere.speechplaning.data.usecases.speaker
+
+import de.geosphere.speechplaning.data.repository.SpeakerRepository
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.result.shouldBeFailure
+import io.kotest.matchers.result.shouldBeSuccess
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+
+class DeleteSpeakerUseCaseTest : BehaviorSpec({
+
+    lateinit var repository: SpeakerRepository
+    lateinit var useCase: DeleteSpeakerUseCase
+
+    beforeTest {
+        repository = mockk()
+        useCase = DeleteSpeakerUseCase(repository)
+    }
+
+    given("a request to delete a speaker") {
+        val districtId = "district1"
+        val congregationId = "congregation1"
+        val speakerId = "speaker1"
+
+        `when`("the repository deletes the speaker successfully") {
+            then("it should return success") {
+                coEvery { repository.deleteSpeaker(districtId, congregationId, speakerId) } returns Unit
+
+                val result = useCase(districtId, congregationId, speakerId)
+
+                result.shouldBeSuccess()
+                coVerify(exactly = 1) { repository.deleteSpeaker(districtId, congregationId, speakerId) }
+            }
+        }
+
+        `when`("the repository throws an exception") {
+            then("it should return failure") {
+                val exception = RuntimeException("Database error")
+                coEvery { repository.deleteSpeaker(districtId, congregationId, speakerId) } throws exception
+
+                val result = useCase(districtId, congregationId, speakerId)
+
+                result.shouldBeFailure(exception)
+                coVerify(exactly = 1) { repository.deleteSpeaker(districtId, congregationId, speakerId) }
+            }
+        }
+    }
+})
---
+++ data/src/test/java/de/geosphere/speechplaning/data/usecases/speaker/GetSpeakersUseCaseTest.kt
@@ -0,0 +1,50 @@
+package de.geosphere.speechplaning.data.usecases.speaker
+
+import de.geosphere.speechplaning.core.model.Speaker
+import de.geosphere.speechplaning.data.repository.SpeakerRepository
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.result.shouldBeFailure
+import io.kotest.matchers.result.shouldBeSuccess
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+
+class GetSpeakersUseCaseTest : BehaviorSpec({
+
+    lateinit var repository: SpeakerRepository
+    lateinit var useCase: GetSpeakersUseCase
+
+    beforeTest {
+        repository = mockk()
+        useCase = GetSpeakersUseCase(repository)
+    }
+
+    given("a request to get all speakers for a congregation") {
+        val districtId = "district1"
+        val congregationId = "congregation1"
+
+        `when`("the repository returns a list of speakers") {
+            then("it should return a success result with the speaker list") {
+                val speakers = listOf(Speaker(id = "1", nameFirst = "John"))
+                coEvery { repository.getSpeakersForCongregation(districtId, congregationId) } returns speakers
+
+                val result = useCase(districtId, congregationId)
+
+                result.shouldBeSuccess(speakers)
+                coVerify(exactly = 1) { repository.getSpeakersForCongregation(districtId, congregationId) }
+            }
+        }
+
+        `when`("the repository throws an exception") {
+            then("it should return a failure result") {
+                val exception = RuntimeException("Database error")
+                coEvery { repository.getSpeakersForCongregation(districtId, congregationId) } throws exception
+
+                val result = useCase(districtId, congregationId)
+
+                result.shouldBeFailure(exception)
+                coVerify(exactly = 1) { repository.getSpeakersForCongregation(districtId, congregationId) }
+            }
+        }
+    }
+})
