384d1ef3747c17816d07e9973e4573b4253aa647
Werner Schröter <werner.schroeter@geosphere.de>
2026-01-11 15:10:43 +0100

    SPEECHPLAN-48 show only contenst with grandet ruelsets  whatsApp calling enabled - fehlerbehebung, visibillity ist jetzt aktualisiert - initial commit

--- feature/login/src/main/java/de/geosphere/speechplaning/feature/login/di/LoginModule.kt
+++ feature/login/src/main/java/de/geosphere/speechplaning/feature/login/di/LoginModule.kt
@@ -1,10 +1,12 @@
 package de.geosphere.speechplaning.feature.login.di
 
 import de.geosphere.speechplaning.feature.login.ui.AuthViewModel
+import de.geosphere.speechplaning.feature.login.ui.UserViewModel
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val loginModule = module {
     // Diese Definition gehört hierher, weil das ViewModel Teil des Login-Features ist.
     viewModelOf(::AuthViewModel)
+    viewModelOf(::UserViewModel)
 }
