667bcf6539071869b932561ccc3f74d7be610b89
Werner Schröter <werner.schroeter@geosphere.de>
2026-06-19 22:44:22 +0200

    JDI: update bottom navigation icons and add new vector resources - add AppEventIconProvider and integrate event icons into congregation event list

--- app/src/main/java/de/geosphere/speechplaning/di/AppModule.kt
+++ app/src/main/java/de/geosphere/speechplaning/di/AppModule.kt
@@ -1,9 +1,11 @@
 package de.geosphere.speechplaning.di
 
+import de.geosphere.speechplaning.core.ui.provider.AppEventIconProvider
 import de.geosphere.speechplaning.core.ui.provider.AppEventStringProvider
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 val appModule = module {
     singleOf(::AppEventStringProvider)
+    singleOf(::AppEventIconProvider)
 }
